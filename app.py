8879dd25ba6825ca5723ab901398184ad07d34a1
woshiyyya <1085966850@qq.com>
2021-12-06 16:57:51 -0500

    merge time range slide bar

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import stat_result
 from numpy import add
 import streamlit as st
 import pandas as pd
@@ -15,14 +16,22 @@ import plotly.graph_objects as go
 def load_dataset():
     df = pd.read_csv("data/data.csv")
     df = df[pd.notnull(df['longitude']) & pd.notnull(df['latitude'])]
+    df['date'] = df.apply(lambda x: int(x['date'].replace('-', '')), axis=1)
     return df
 
 
 @st.cache
-def load_geo_subset(df):
-    geo_df = df[["longitude", "latitude"]]
+def load_geo_subset(df, start_date, end_date):
+    geo_df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
+    geo_df = geo_df[["longitude", "latitude"]]
     geo_df.dropna(inplace=True)
     return geo_df
+
+@st.cache
+def load_cities_subset(df, start_date, end_date):
+    cities_df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
+    cities = cities_df['city_or_county'].value_counts()[:20][::-1]
+    return cities
 
 
 @st.cache
@@ -31,20 +40,49 @@ def city_list(df):
 
 
 @st.cache
-def load_city_subset(df, city):
+def load_city_subset(df, city, start_date, end_date):
     subdf = df[df['city_or_county'] == city].copy()
+    subdf = subdf[(subdf['date'] >= start_date) & (subdf['date'] <= end_date)]
     subdf = subdf[["longitude", "latitude",
                    "address", "n_killed", "n_injured"]]
     subdf.dropna(inplace=True)
     return subdf
 
+@st.cache
+def load_date_subset(df, date):
+    subdf = df[df['date'].str.contains(date[:7])].copy()
+    return subdf
 
+
+@st.cache
+def get_user_mapping(element):
+    if element == "NA":
+        return {}
+    mapping = {}
+    for d in element.split("||"):
+        try:
+            key = d.split("::")[0]
+            val = d.split("::")[1]
+            if key not in mapping:
+                mapping[key] = val
+        except:
+            pass
+    return mapping
+
+
+@st.cache
+def get_unique_name(df, column_name):
+    s = set()
+    for item in df[column_name]:
+        for k, v in item.items():
+            s.add(v)
+    return s
 class AppLayout:
     def __init__(self) -> None:
         self.df = load_dataset()
 
     def make_country_map(self):
-        geo_df = load_geo_subset(self.df)
+        geo_df = load_geo_subset(self.df, self.start_date, self.end_date)
         col1, _ = st.columns([4, 1])
         with col1:
             st.pydeck_chart(pdk.Deck(
@@ -69,7 +107,7 @@ class AppLayout:
     def make_city_map(self):
         select_city = st.selectbox(
             label='Choose a city:', options=city_list(self.df))
-        city_df = load_city_subset(self.df, select_city)
+        city_df = load_city_subset(self.df, select_city, self.start_date, self.end_date)
         col1, col2 = st.columns([5, 2])
         with col1:
             init_lng = city_df["longitude"].median()
@@ -97,7 +135,7 @@ class AppLayout:
             )
             )
         with col2:
-            cities = self.df['city_or_county'].value_counts()[:20][::-1]
+            cities = load_cities_subset(self.df, self.start_date, self.end_date)
             fig = go.Figure()
             fig.add_trace(go.Bar(x=cities, 
                                  y=cities.index, 
@@ -117,17 +155,28 @@ class AppLayout:
                                     t=50,
                                 ), width=400, height=550)
             st.plotly_chart(fig)
+    
+    def set_date_range(self, start_date, end_date):
+        self.start_date = int(start_date.strftime("%Y%m%d"))
+        self.end_date = int(end_date.strftime("%Y%m%d"))
             
 class MainApp(HydraHeadApp):
     def __init__(self) -> None:
         self._app = AppLayout()
 
     def run(self):
+        start_date, end_date = st.sidebar.slider(label="Choose time range",
+                                        value=(datetime.date(2013, 1, 1), datetime.date(2018, 3, 31)), 
+                                        key="all_data", 
+                                        min_value=datetime.date(2013, 1, 1), 
+                                        max_value=datetime.date(2018, 3, 31))
+        self._app.set_date_range(start_date, end_date)
+        add_sidebar()
         st.header("Geographical distribution of Gun Shots in the U.S.")
         self._app.make_country_map()
         st.header("City-wise Gun Shots Browser")
         self._app.make_city_map()
-        add_sidebar()
+    
 
 
 if __name__ == "__main__":
